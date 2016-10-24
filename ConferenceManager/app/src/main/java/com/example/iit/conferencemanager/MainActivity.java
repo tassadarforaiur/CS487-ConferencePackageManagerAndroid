2b664d1b58cb073e427421ce62b5cff648ecc0c7
tassadarforaiur <dificil1>
2016-10-24 18:06:35 -0500

    added login

--- ConferenceManager/app/src/main/java/com/example/iit/conferencemanager/MainActivity.java
+++ ConferenceManager/app/src/main/java/com/example/iit/conferencemanager/MainActivity.java
@@ -18,4 +18,9 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(MainActivity.this, Signup.class);
         startActivity(intent);
     }
+    public void sendMessage1(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, Login.class);
+        startActivity(intent);
+    }
 }
